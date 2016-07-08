9f9273ea29a0808b5b30b9f5f0d57e44a37daad4
NhatVu <minhnhatse.uit@gmail.com>
2016-07-08 21:43:11 +0700

    Add timestamp on UI

---
+++ debugger.js
@@ -0,0 +1,6 @@
+var a = 10;
+var b = 15;
+
+debugger;
+var c = a+b;
+console.log(c);
--- server.js
+++ server.js
@@ -3,21 +3,24 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var moment = require('moment');
 
 app.use(express.static('./public'));
 
 io.on('connection', function(socket){
+	debugger;
 	console.log('User connected via socket.io!');
 
 	socket.on('message', function(message){
 		console.log('Server received: ' + message.text);
-
+		message.timestamp = moment.valueOf();
 		//socket.broadcast.emit('message', message);
 		io.emit('message', message);
 	});
 
 	socket.emit('message', {
-		text: 'Welcome to chat application!'
+		text: 'Welcome to chat application!',
+		timestamp: moment.valueOf()
 	});
 });
 
